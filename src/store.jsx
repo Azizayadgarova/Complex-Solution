c5362333b5f745a93d211080007750af77f6bc4c
yadgarovtthjjiiiiiiiiiiiiiiiiiiii <mamuray819@gmail.com>
2025-07-07 16:30:03 +0500

    done

--- src/store.jsx
+++ src/store.jsx
@@ -1,27 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "./layout/MainLayout";
+
+// Sahifalar
+import Home from "./rautes/Home";
 import Abaute from "./rautes/Abaute";
 import Blog from "./rautes/Blog";
 import Contact from "./rautes/Contact";
-import Home from "./rautes/Home";
 import Portfolio from "./rautes/Portfolio";
 import Serevises from "./rautes/Serevises";
 
-// Portfolio components
-import PortfolioAll from "./Componets/portfolioAll";
-import PortfolioLife from "./Componets/PortfolioLife";
-import PortfolioMoments from "./Componets/PortfolioMoments";
-import PortfolioNature from "./Componets/PortfolioNature";
-import PortfolioTravel from "./Componets/PortfolioTravel";
-import PortfolioEducation from "./Componets/PortfolioEducation";
-import PortfolioSector from "./Componets/PortfolioSector";
+// Portfolio componentlari
+import PortfolioAll from "./Components/PortfolioAll";
+import PortfolioLife from "./Components/PortfolioLife";
+import PortfolioMoments from "./Components/PortfolioMoments";
+import PortfolioNature from "./Components/PortfolioNature";
+import PortfolioTravel from "./Components/PortfolioTravel";
+import PortfolioEducation from "./Components/PortfolioEducation";
+import PortfolioSector from "./Components/PortfolioSector";
 
-// Advantage components
-import Advantage1 from "./Componets/Advantage1";
-import Advantage2 from "./Componets/Advantage2";
-import Advantage3 from "./Componets/Advantage3";
-import Advantage4 from "./Componets/Advantage4";
-import Advantage5 from "./Componets/Advantage5";
+// Advantage (xizmatlar) componentlari
+import Advantage1 from "./Components/Advantage1";
+import Advantage2 from "./Components/Advantage2";
+import Advantage3 from "./Components/Advantage3";
+import Advantage4 from "./Components/Advantage4";
+import Advantage5 from "./Components/Advantage5";
 
 const router = createBrowserRouter([
   {
@@ -29,10 +31,12 @@ const router = createBrowserRouter([
     element: <MainLayout />,
     children: [
       { index: true, element: <Home /> },
+
       {
         path: "aboute",
         element: <Abaute />,
       },
+
       {
         path: "services",
         element: <Serevises />,
@@ -44,6 +48,7 @@ const router = createBrowserRouter([
           { path: "four", element: <Advantage5 /> },
         ],
       },
+
       {
         path: "portfolio",
         element: <Portfolio />,
@@ -57,10 +62,12 @@ const router = createBrowserRouter([
           { path: "portfoliosector", element: <PortfolioSector /> },
         ],
       },
+
       {
         path: "blog",
         element: <Blog />,
       },
+
       {
         path: "contact",
         element: <Contact />,
